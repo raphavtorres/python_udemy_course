8fdc1dc86072eac72c2e87228f0c1826e4f3eace
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-03-09 16:51:24 -0300

    Add files via upload

---
+++ validadorCPF.py
@@ -0,0 +1,13 @@
+'''
+    746.824.890-70 (746824890)
+'''
+
+cpf = input("Digite seu CPF: ")
+# Multiplicando cada um dos 9 primeiros valores por contagem regressiva iniciada em 10
+
+list_cpf_nove_dig = cpf.split('-')[0]
+cpf_nove_dig_clean = list_cpf_nove_dig.replace(".", "")
+
+
+print(...)
+
