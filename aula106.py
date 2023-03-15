e8736176ad37776544fb01b62c8e0b338fc31a5f
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-03-15 16:57:31 -0300

    Add files via upload

---
+++ aula106.py
@@ -0,0 +1,5 @@
+def soma(x, y):  # definição/nome/execução
+    print(f"{x=}, y={y} | {x + y = }")
+
+
+soma(1, 2)
---
+++ aula107.py
@@ -0,0 +1,15 @@
+# 0 é um false value
+# Por isso eu uso None, pois a pessoa podia passar 0 mas n ia ser printado
+# Neste caso eu testo se estou ou não recebendo o "z"
+
+def soma(x, y, z=None):
+    if z is not None:
+        print(f'{x=} {y=} {z=}', x + y + z)
+    else:
+        print(f'{x=} {y=}', x + y)
+
+
+soma(1, 2)
+soma(3, 5)
+soma(100, 200)
+soma(7, 9, 0)
