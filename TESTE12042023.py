65e43c23e6bd9e35d6e54a65f137e45e430eb1bb
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-04-12 16:20:15 -0300

    Update TESTE12042023.py

--- TESTE12042023.py
+++ TESTE12042023.py
@@ -16,3 +16,37 @@ with open(path_file, 'w') as file:
     print('And close file')
 
 print(path_file)
+
+
+
+
+
+path_file = 'C:\\Users\\ct67ca\\Desktop\\pyhtonTorres\\'
+path_file += 'file.txt'
+
+
+with open(path_file, 'w+') as file:
+    # WRITING IN FILE
+    file.write('Line 1\n')
+    file.write('Line 1\n')
+    # write multiple lines ==> receives a iterable
+    file.writelines(
+        ('Line 3\n', 'Line 4\n')
+    )
+
+    # READING FILE
+    file.seek(0, 0)  # move the cursor up to read the content
+    print(file.read())
+    print('Reading...')
+    file.seek(0, 0)
+    print(file.readline())
+
+    # READING MULTIPLE LINES
+    print('READ LINES')
+    for line in file.readlines():
+        print(line.strip())
+
+print('-' * 40)
+
+with open(path_file, 'r') as file:
+    print(file.read())
