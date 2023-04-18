40dc8461b4740372eee200f1041aac87bec54c88
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-04-18 16:20:53 -0300

    Update 18-04-23.py

--- 18-04-23.py
+++ 18-04-23.py
@@ -19,3 +19,37 @@ pen = Pen()
 pen.color = 'pink'
 # getter
 print(pen.color)
+
+
+
+####################################################
+
+
+# Encapsulation
+# Python doesn't have access modifier
+
+class Foo:
+    def __init__(self):
+        self.public = 'this is public'
+        self._protected = 'this is protected'
+        self.__private = 'this is private'
+
+    def public_method(self):
+        self._protected_method()
+        print(self.__private)
+        return 'public method'
+
+    def _protected_method(self):
+        print('_protected_method')
+        return 'protected method'
+
+    def __private_method(self):
+        print('&&&&__private_method')
+        return 'private method'
+
+
+f = Foo()
+# print(f.public)
+# print(f._protected)  # that's not right, shouldn't be possible - but names with underscore aren't imported
+print(f.public_method())
+print(f._Foo__private_method())  # name mangling --> name defacement
